9e67b61f5b39c4c45abd3e4c5e9b0f322c3b343e
Luka <lsvast@gmail.com>
2017-03-25 19:03:11 +0100

    World Tour update

--- projects/WorldTour/js/script.js
+++ projects/WorldTour/js/script.js
@@ -1,22 +1,84 @@
+function myMap(){
+
+  var mapOptions= {
+    center:new google.maps.LatLng(45,13),
+    zoom:3,
+    scrollwheel: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    overviewMapControl: false,
+    rotateControl: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  google.maps.event.addListener(map, "click", function(event) {
+      var lat = event.latLng.lat();
+      var lng = event.latLng.lng();
+      // populate yor box/field with lat, lng
+      //alert("Lat=" + lat + "; Lng=" + lng);
+      $("#places").append("<button class='w3-bar-item w3-button w3-blue w3-center' onclick='searchByCoo(this);' data-lat='"+lat+"' data-lng='"+lng+"'>Discover</button>")
+      placeMarker(event.latLng, map);
+  });
+  function placeMarker(position, map) {
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
+    });
+  }
+}
+
+function clearData() {
+  $("#places").html("");
+  $("#photo_grid").html("");
+  marker.setMap(null);
+}
+
 // Script to open and close sidebar
 function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
+    $("#mySidebar").show();
+    $("#myOverlay").show();
 }
 function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
+  $("#mySidebar").hide();
+  $("#myOverlay").hide();
 }
 
 // Modal Image Gallery
 function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-  var captionText = document.getElementById("caption");
-  captionText.innerHTML = element.alt;
+  $("#img").attr('src',element.src);
+  //console.log(element.src);
+  $("#modal").show();
 }
 
 var api_key="349a4881349e36583f3d5b02876c8791";
+
+//search for photos based on coordinates
+function searchByCoo(element){
+  //alert($(element).attr("data-lat"));
+  var lat=$(element).attr("data-lat");
+  var lng=$(element).attr("data-lng");
+  $(element).addClass("w3-green");
+  //alert(lat+lng);
+  $("#photo_grid").html("");
+
+  var data = {"method": "flickr.places.findByLatLon", "api_key": api_key, "lat": lat, "lon": lng, "format": "json", "nojsoncallback": "1"};
+
+  //AJAX call to Flicker places find
+  $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: "https://api.flickr.com/services/rest/",
+      data: data,
+      success: function(data,status) {
+        place_id=data.places.place[0].place_id;
+        //console.log(data.places);
+        getPhoto(place_id);
+        $(element).text(data.places.place[0].woe_name);
+      }
+  });
+}
 
 // search for photos
 function searchPhotos() {
@@ -34,6 +96,7 @@ function searchPhotos() {
       data: data,
       success: function(data,status) {
         place_id=data.places.place[0].place_id;
+        console.log(data.places);
         getPhoto(place_id);
       }
   });
@@ -69,7 +132,7 @@ function displayPhoto(photo_id) {
         data: data,
         success: function(data,status) {
           img_src=data.sizes.size[5].source;
-          $("#photo_grid").append('<div class="w3-third"><img src="'+img_src+'" style="width:100%" onclick="onClick(this)" alt="Description."></div>')
+          $("#photo_grid").append('<div class="w3-third"><img src="'+img_src+'" style="width:100%" onclick="onClick(this)"></div>')
         }
     });
 
